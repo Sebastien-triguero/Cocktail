e217f624d724750b2bdcee6bc0ddd5477a1f0361
Tsuyuky <sebastien.triguero@hotmail.fr>
2025-03-20 19:01:50 +0100

    version 1 du projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './components/header.component';
+import {CocktailDetailsComponent} from './components/cocktails/components/cocktail-details.component';
+import {FooterComponent} from './components/footer.component';
+import {CocktailsComponent} from './components/cocktails/cocktails.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    HeaderComponent,
+    CocktailsComponent,
+    FooterComponent,
+  ],
+  template: `
+  <app-header/>
+  <app-cocktails class="flex-auto"/>
+  <app-footer/>
+  `,
+  styles: `
+  :host {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }`
 })
 export class AppComponent {
   title = 'ProjetCocktails';
